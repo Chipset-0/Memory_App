7f1cd824e1310451f5c43baf05b7093f424779fa
Chipset-0 <zachariahdennett@gmail.com>
2025-06-18 23:04:35 +0800

    Added PC Box Styling. Imports 30 random pokemon sprites upon start. Need to add click functionality.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Card.css'
 
-export default function Card({imageLink, index, clickFunction})
+export default function Card({pokeId, clickFunction, hidden=false})
 {
+    const [imageUrl, setImageUrl] = useState("")
+    
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`).then(response => response.json())
+        .then(data => {
+            setImageUrl(data.sprites.front_default)
+        })
+        .catch(error => console.error("Error fetching Pokémon data:", error))
+    })
+
     return (
-            <div class="card-container">
-                <img src={imageLink} class="card-image" />
+            <div className="card-container">
+                {imageUrl ? <img src={imageUrl} className={`card-image ${hidden ? "hide" : ""}`} onClick={() => clickFunction(pokeId)}/> : <p>Loading</p>}
             </div>
     )
 }
--- src/Components/List/CardList.jsx
+++ src/Components/List/CardList.jsx
@@ -2,21 +2,48 @@ import { useState, useEffect } from 'react'
 import './CardList.css'
 import Card from '../Card/Card'
 
-export default function CardList({category})
-{
-    const importAllImages = (requireContext) => requireContext.keys().map(requireContext)
+const TOTAL_CARDS = 1025
+const VISIBLE_AMOUNT = 30
 
-    const useImageArray = (directory) => {
-        const [images, setImages] = useState([])
+function shuffle(array)
+{
+    const arr = array.slice();
+    for (let i = arr.length - 1; i > 0; i--)
+    {
+        const j = Math.floor(Math.random() * (i+1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+export default function CardList({category, cardAmount=30})
+{
+    const [cards, setCards] = useState([])
+    
+    useEffect(() => {
+        const availId = Array.from({length : TOTAL_CARDS}, (_, i) => i+1);
+        const shuffled = shuffle(availId);
+        const selected = shuffled.slice(0, cardAmount)
+
+        const indices = Array.from({ length: TOTAL_CARDS }, (_, i) => i);
+        const shuffledInd = shuffle(indices);
+
+        const generatedCards = selected.map((id, idx) => ({
+        pokeId: id,
+        cardIndex: idx
+        }));
+
+        setCards(generatedCards);
+    }, [cardAmount]);
+    
+    const onClickFunc = (clicked) => {
+        console.log(clicked)
     }
 
-    useEffect(() => {
-        const images = importAllImages
-    })
-
+    console.log(cards)
     return (
-        <div className='card-list-container' onClick={() => clickFunction}>
-            <Card image=""/>
+        <div className='card-list-container'>
+            {cards.map((card, index) => (<Card key={index} pokeId={card.pokeId} clickFunction={onClickFunc}/>))}
         </div>
     )
 }
